1f4d68c3e024dbab69a9b010f956a546a1e58b50
EngineGames-Development <LeanderGamer2@web.de>
2026-07-28 02:46:48 +0200

    Terminal

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,6 +2,8 @@
 #include "include/font.h"
 
 #define NULL ((void *)0)
+#define TERM_ROWS 21
+#define TERM_COLS 72
 
 unsigned int *framebuffer = NULL;
 unsigned int screen_width = 1024;
@@ -12,8 +14,9 @@ int term_box_y = 150;
 int term_box_width = 600;
 int term_box_height = 400;
 
-int cursor_x = 222;
-int cursor_y = 185;
+char terminal_buffer[TERM_ROWS][TERM_COLS];
+int current_row = 0;
+int current_col = 0;
 
 char input_buffer[64];
 int input_length = 0;
@@ -58,74 +61,85 @@ void draw_window(int x, int y, int w, int h, const char *title) {
 
 void draw_desktop_and_taskbar(void) {
   clear_screen_gui(0x001A1A24);
-
   unsigned int taskbar_height = 40;
   unsigned int taskbar_y = screen_height - taskbar_height;
   draw_rect(0, taskbar_y, screen_width, taskbar_height, 0x00101014);
-
   unsigned int button_width = 60;
   unsigned int button_height = 28;
   unsigned int button_x = 6;
   unsigned int button_y = taskbar_y + ((taskbar_height - button_height) / 2);
   draw_rect(button_x, button_y, button_width, button_height, 0x00005B9E);
-
   draw_string(button_x + 10, button_y + 6, "START", 0x00FFFFFF);
   draw_string(20, 20, "SECUREOS NEXT GEN", 0x00005B9E);
-
   draw_window(term_box_x, term_box_y, term_box_width, term_box_height,
               "CONSOLE TERMINAL");
 }
 
-void clear_screen(void) {
-  draw_desktop_and_taskbar();
-  cursor_x = term_box_x + 12;
-  cursor_y = term_box_y + 40;
+void redraw_terminal_text(void) {
+  draw_rect(term_box_x + 6, term_box_y + 34, term_box_width - 12,
+            term_box_height - 40, 0x000A0A0F);
+  for (int r = 0; r < TERM_ROWS; r++) {
+    for (int c = 0; c < TERM_COLS; c++) {
+      if (terminal_buffer[r][c] == '\0')
+        continue;
+      draw_char(term_box_x + 12 + (c * 8), term_box_y + 40 + (r * 16),
+                terminal_buffer[r][c], 0x00FFFFFF);
+    }
+  }
 }
 
-void trigger_kernel_panic_gui(const char *error_msg) {
-  clear_screen_gui(0x00AA0000);
-  draw_rect(50, 50, screen_width - 100, 40, 0x001A1A24);
-  draw_string(70, 62, "!!! KERNEL PANIC !!!", 0x00FF0000);
-
-  draw_string(50, 130, "A critical system error has occurred.", 0x00FFFFFF);
-  draw_string(50, 160, "Error code:", 0x00FFFFFF);
-  draw_string(150, 160, error_msg, 0x00FFFFFF);
-  draw_string(50, 210, "The system has been halted to prevent damage.",
-              0x00FFFFFF);
-
-  while (1) {
-    asm volatile("hlt");
+void scroll_terminal(void) {
+  for (int r = 0; r < TERM_ROWS - 1; r++) {
+    for (int c = 0; c < TERM_COLS; c++) {
+      terminal_buffer[r][c] = terminal_buffer[r + 1][c];
+    }
   }
+  for (int c = 0; c < TERM_COLS; c++) {
+    terminal_buffer[TERM_ROWS - 1][c] = '\0';
+  }
+  current_row = TERM_ROWS - 1;
+  current_col = 0;
+}
+
+void clear_screen(void) {
+  for (int r = 0; r < TERM_ROWS; r++) {
+    for (int c = 0; c < TERM_COLS; c++) {
+      terminal_buffer[r][c] = '\0';
+    }
+  }
+  current_row = 0;
+  current_col = 0;
+  draw_desktop_and_taskbar();
 }
 
 void print_char(char c) {
-  int max_x = term_box_x + term_box_width - 20;
-  int max_y = term_box_y + term_box_height - 20;
-
   if (c == '\n') {
-    cursor_x = term_box_x + 12;
-    cursor_y += 16;
+    current_row++;
+    current_col = 0;
+    if (current_row >= TERM_ROWS) {
+      scroll_terminal();
+    }
+    redraw_terminal_text();
     return;
   }
   if (c == '\b') {
-    if (cursor_x > term_box_x + 12) {
-      cursor_x -= 8;
-      draw_rect(cursor_x, cursor_y, 8, 16, 0x000A0A0F);
+    if (current_col > 0) {
+      current_col--;
+      terminal_buffer[current_row][current_col] = '\0';
+      redraw_terminal_text();
     }
     return;
   }
-
-  if (cursor_y >= max_y) {
-    clear_screen();
+  if (current_col >= TERM_COLS) {
+    current_row++;
+    current_col = 0;
+    if (current_row >= TERM_ROWS) {
+      scroll_terminal();
+    }
   }
-
-  draw_char(cursor_x, cursor_y, c, 0x00FFFFFF);
-  cursor_x += 8;
-
-  if (cursor_x >= max_x) {
-    cursor_x = term_box_x + 12;
-    cursor_y += 16;
-  }
+  terminal_buffer[current_row][current_col] = c;
+  current_col++;
+  redraw_terminal_text();
 }
 
 void print_string(const char *str) {
@@ -142,24 +156,33 @@ void sleep_ms(int milliseconds) {
   }
 }
 
+void trigger_kernel_panic_gui(const char *error_msg) {
+  clear_screen_gui(0x00AA0000);
+  draw_rect(50, 50, screen_width - 100, 40, 0x001A1A24);
+  draw_string(70, 62, "!!! KERNEL PANIC !!!", 0x00FF0000);
+  draw_string(50, 130, "A critical system error has occurred.", 0x00FFFFFF);
+  draw_string(50, 160, "Error code:", 0x00FFFFFF);
+  draw_string(150, 160, error_msg, 0x00FFFFFF);
+  draw_string(50, 210, "The system has been halted to prevent damage.",
+              0x00FFFFFF);
+  while (1) {
+    asm volatile("hlt");
+  }
+}
+
 void start_graphics_terminal(unsigned int *multiboot_info) {
   (void)multiboot_info;
   framebuffer = (unsigned int *)0xFD000000;
   screen_width = 1024;
   screen_height = 768;
-
-  draw_desktop_and_taskbar();
-
+  clear_screen();
   print_string("SECUREOS READY.\n> ");
-
   while (1) {
     unsigned char status = 0;
     asm volatile("inb $0x64, %0" : "=a"(status));
-
     if (status & 1) {
       unsigned char scancode = 0;
       asm volatile("inb $0x60, %0" : "=a"(scancode));
-
       if (!(scancode & 0x80)) {
         char c = keyboard_map[scancode];
         if (c != 0) {
@@ -180,5 +203,6 @@ void start_graphics_terminal(unsigned int *multiboot_info) {
         }
       }
     }
+    asm volatile("hlt");
   }
 }
